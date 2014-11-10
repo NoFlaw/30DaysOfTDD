aa4ee012c17ff5d965f272c88edeb5da9bbc4ca1
NoFlaw <brentonbates@gmail.com>
2014-11-10 13:10:04 -0500

    Adding code to get back to Day 9 of ThirtyDaysOfTDD

--- 30DaysOfTDD.UnitTests/StringUtils.cs
+++ 30DaysOfTDD.UnitTests/StringUtils.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _30DaysOfTDD.UnitTests
 {
-    class StringUtils
+    public class StringUtils
     {
+        public int FindNumberOfOccurences(string sentenceToScan, string characterToScanFor)
+        {
+
+            if (characterToScanFor.Length != 1 || string.IsNullOrEmpty(characterToScanFor))
+                throw new ArgumentException();
+
+            var numberOfOccurenes = 0;
+            var characterToCheckFor = Char.Parse(characterToScanFor);
+            var stringToCheckAsCharacterArray = sentenceToScan.ToCharArray();
+
+            for (var charIdx = 0; charIdx < stringToCheckAsCharacterArray.GetUpperBound(0); charIdx++)
+            {
+                if (stringToCheckAsCharacterArray[charIdx] == characterToCheckFor)
+                {
+                    numberOfOccurenes++;
+                }
+            }
+
+            return numberOfOccurenes;
+        }
     }
 }
--- 30DaysOfTDD.UnitTests/StringUtilsTests.cs
+++ 30DaysOfTDD.UnitTests/StringUtilsTests.cs
@@ -1,12 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace _30DaysOfTDD.UnitTests
 {
-    class StringUtilsTests
+    [TestFixture]
+    public class StringUtilsTest
     {
+        private StringUtils _stringUtils;
+
+        [TestFixtureSetUp]
+        public void SetupStringUtilTests()
+        {
+            _stringUtils = new StringUtils();
+        }
+
+
+        [Test]
+        public void ShouldBeAbleToCountNumberOfLettersInSimpleSentence()
+        {
+            const int expectedResult = 2;
+            const string characterToScanFor = "e";
+            const string sentenceToScan = "TDD is awesome!";
+
+            var result = _stringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void ShouldBeAbleToCountNumberOfLettersInAComplexSentence()
+        {
+            const int expectedResult = 5;
+            const string characterToScanFor = "n";
+            const string sentenceToScan = "Once is unique, twice is a coincidence, three times is a pattern.";
+
+            var result = _stringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldGetAnArgumentExceptionWhenCharacterToScanForIsLargerThanOneCharacter()
+        {
+            const string sentenceToScan = "This test should throw an exception";
+            const string characterToScanFor = "xx";
+
+            _stringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor);
+        }
+
+
     }
 }
